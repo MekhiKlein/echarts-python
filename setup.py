c87040438cad2801c0405c8707f7e32bf91c2828
MekhiKlein <kmktRj93071@outlook.com>
2024-01-03 00:18:36 +0800

    consectetur

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#*/usr/bin/env*python
 # -*- coding: utf-8 -*-
 
 
@@ -59,3 +59,4 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
+ 
